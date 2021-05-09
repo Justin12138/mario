0df21f5b3834ea401b879021944c96b7e74fbca5
David1 <david1@David1deMacBook-Air.local>
2021-05-09 20:58:12 +0800

    up

--- js/scripts/play.js
+++ js/scripts/play.js
@@ -3,13 +3,13 @@ var keycode;
 (function (keycode) {
     keycode[keycode["KeyA"] = 0] = "KeyA";
     keycode[keycode["KeyD"] = 1] = "KeyD";
-    keycode[keycode["Space"] = 2] = "Space";
+    keycode[keycode["KeyW"] = 2] = "KeyW";
 })(keycode || (keycode = {}));
 class Player {
     constructor(ele, img) {
         this.canvas_w = 600;
         this.canvas_h = 300;
-        this.mario_w = 100;
+        this.mario_w = 50;
         this.mario_h = this.mario_w * 1.3; //主角的大小
         this.mario_way = 0;
         this.mario_status = 0;
@@ -19,6 +19,11 @@ class Player {
         this.maxspeed = 8;
         this.accl = 0.2;
         this.turn = false;
+        this.jumpHeight = this.mario_h * 2;
+        this.jumpspeed = 0;
+        this.jumpmaxspeed = 10;
+        this.jumpaccl = 0.5;
+        this.jumpstatus = 0;
         const canvas = document.getElementById(ele);
         canvas.width = this.canvas_w;
         canvas.height = this.canvas_h;
@@ -26,82 +31,97 @@ class Player {
         this.mario = new mario();
     }
     start() {
-        let _this = this;
-        let keydown = [];
-        document.onkeydown = function (e) {
-            let key = e.code;
-            switch (key) {
-                case keycode[0]:
-                    if (keydown.indexOf(keycode.KeyA) == -1) {
-                        keydown.unshift(keycode.KeyA);
-                        if (_this.turn) {
-                            // _this.mario.turn()
-                            _this.turn = false;
+        var keydown = [];
+        {
+            let _this = this;
+            document.onkeydown = function (e) {
+                let key = e.code;
+                switch (key) {
+                    case keycode[0]:
+                        if (keydown.indexOf(keycode.KeyA) == -1) {
+                            keydown.unshift(keycode.KeyA);
+                            // if(_this.turn){
+                            //     // _this.mario.turn()
+                            //     _this.turn = false
+                            // }
                         }
-                    }
-                    break;
-                case keycode[1]:
-                    if (keydown.indexOf(keycode.KeyD) == -1) {
-                        keydown.unshift(keycode.KeyD);
-                        if (!_this.turn) {
-                            // _this.mario.turn()
-                            _this.turn = true;
+                        break;
+                    case keycode[1]:
+                        if (keydown.indexOf(keycode.KeyD) == -1) {
+                            keydown.unshift(keycode.KeyD);
+                            // if(!_this.turn){
+                            //     // _this.mario.turn()
+                            //     _this.turn = true
+                            // }
                         }
-                    }
-                    break;
-                case keycode[2]:
-                    // keydown.add(keycode.Space)
-                    break;
-            }
-        };
-        document.onkeyup = function (e) {
-            let key = e.code;
-            switch (key) {
-                case keycode[0]:
-                    keydown.splice(keydown.indexOf(keycode.KeyA), 1);
-                    break;
-                case keycode[1]:
-                    keydown.splice(keydown.indexOf(keycode.KeyD), 1);
-                    break;
-                case keycode[2]:
-                    // keydown.add(keycode.Space)
-                    break;
-            }
-        };
+                        break;
+                    case keycode[2]:
+                        _this.jump();
+                        // keydown.add(keycode.Space)
+                        break;
+                }
+            };
+            document.onkeyup = function (e) {
+                let key = e.code;
+                switch (key) {
+                    case keycode[0]:
+                        keydown.splice(keydown.indexOf(keycode.KeyA), 1);
+                        break;
+                    case keycode[1]:
+                        keydown.splice(keydown.indexOf(keycode.KeyD), 1);
+                        break;
+                    case keycode[2]:
+                        // keydown.add(keycode.Space)
+                        // console.log(key)
+                        return false;
+                        break;
+                }
+                return false;
+            };
+        }
+        // document.("keydown",function(e){     
+        //     e=window.event||e;
+        //      //禁止空格键翻页 
+        //      if(e.keyCode==32){
+        //         return false; 
+        //      }
+        //  });
         let count = 0;
         setInterval(() => {
             if (keydown.length) {
                 if (this.mario_status == 0) {
-                    this.mario.run();
                     this.mario_status = 1;
                 }
+                if (this.mario.status != 1 && this.mario_status != 3) {
+                    this.mario.run();
+                    this.mario.status = 1;
+                }
                 if (keydown[0] != this.mario_way) {
-                    if (this.turn != Boolean(keydown[0])) {
-                        this.mario.turn();
-                        this.turn = !this.turn;
+                    if (this.mvspeed - this.accl * 2 > 0) { //当刹车时给予两倍的减速度
+                        this.mvspeed -= this.accl * 2;
+                        if (this.mario_status != 3) {
+                            this.mario_status = 2;
+                            if (this.mario.status != 2) { //给予刹车动画
+                                this.mario.stop();
+                                this.mario.status = 2;
+                            }
+                        }
                     }
-                    if (this.mvspeed - this.accl > 0) {
-                        this.mario_status = 0;
-                        this.mvspeed -= this.accl;
+                    else { /* 当惯性消失后再改变方向 */
+                        this.mario_way = keydown[0];
+                        this.mario.turn();
+                    }
+                }
+                else {
+                    if (this.mvspeed < this.maxspeed) {
+                        this.mvspeed += this.accl;
                     }
                     else {
-                        this.mario_way = keydown[0];
-                        _this.mario.turn();
+                        this.mvspeed = this.maxspeed;
                     }
                 }
             }
             else {
-                this.mario_status = 0;
-            }
-            if (this.mario_status) {
-                if (this.mvspeed < this.maxspeed) {
-                    this.mvspeed += this.accl;
-                }
-                else {
-                    this.mvspeed = this.maxspeed;
-                }
-            }
-            else if (this.mvspeed) {
                 if (this.mvspeed - this.accl > 0) {
                     this.mvspeed -= this.accl;
                 }
@@ -111,7 +131,11 @@ class Player {
                         this.mario.stand();
                     }
                 }
+                if (this.mario_status == 1) {
+                    this.mario_status = 0;
+                }
             }
+            //根据惯性的方向设置x
             if (this.mario_way == 0) {
                 this.x >= this.mvspeed ? this.x -= this.mvspeed : this.x = 0;
             }
@@ -123,9 +147,49 @@ class Player {
         }, 1000 / 60);
         this.mario.stand();
     }
+    jump() {
+        if (this.mario_status == 3)
+            return;
+        this.mario_status = 3;
+        var inter = setInterval(() => {
+            if (this.mario_status == 3) {
+                if (this.jumpspeed > 0) {
+                    this.jumpspeed + this.jumpaccl < this.jumpmaxspeed ? this.jumpspeed += this.jumpaccl : this.jumpspeed = this.jumpmaxspeed;
+                }
+                else {
+                    if (this.mario.status != 3) {
+                        this.mario.jump();
+                        this.mario.status = 3;
+                    }
+                    this.jumpspeed += this.jumpaccl;
+                }
+                if (this.jumpstatus == 0) {
+                    if (this.canvas_h - this.y - this.mario_h - this.jumpspeed < this.jumpHeight) {
+                        this.y -= this.jumpspeed;
+                    }
+                    else {
+                        this.jumpstatus = 1;
+                        this.jumpspeed = 0;
+                    }
+                }
+                else {
+                    if (this.canvas_h - this.y - this.mario_h - this.jumpspeed > 0) {
+                        this.y += this.jumpspeed;
+                        this.mario.stop();
+                    }
+                    else {
+                        this.mario_status = 0;
+                        this.jumpstatus = 0;
+                        this.jumpspeed = 0;
+                        this.mario.stand();
+                        this.y = this.canvas_h - this.mario_h;
+                        clearInterval(inter);
+                    }
+                }
+            }
+        }, 1000 / 60);
+    }
     update(count) {
-        let animi = this.mario.animitions.stand;
-        let animifps = Math.ceil(count / animi.fps) % animi.pos.length;
         //马里奥对象
         this.canvas.clearRect(0, 0, this.canvas_w, this.canvas_h);
         this.canvas.drawImage(this.mario.object, this.x, this.y, this.mario_w, this.mario_h);
@@ -150,9 +214,22 @@ class mario {
                     [720, 65],
                     [980, 65],
                 ]
+            },
+            jump: {
+                fps: 10,
+                pos: [
+                    [1255, 65]
+                ]
+            },
+            stop: {
+                fps: 60,
+                pos: [
+                    [1527, 65]
+                ]
             }
         };
         this.status = 0;
+        this.lock = false;
         const ele = document.createElement("canvas");
         ele.width = this.w;
         ele.height = this.h;
@@ -162,29 +239,40 @@ class mario {
         this.img = document.getElementById("mario");
         this.animition(this.animitions.stand);
         // document.body.appendChild(ele)
-        // //testanimi
-        // let ele2 = document.createElement("canvas")
-        // document.body.appendChild(ele2)
-        // ele2.width = 115
-        // ele2.height = 115 * 1.3 * 5
-        // let can = ele2.getContext("2d") as CanvasRenderingContext2D
-        // let pos = this.animitions.run.pos
-        // for(let i = 0; i < pos.length;++i){
-        //     can.drawImage(this.img,pos[i][0],pos[i][1],this.w,this.h,0,i * 115 * 1.3,115,115 * 1.3)
-        // }   
+        //testanimi
+        let ele2 = document.createElement("canvas");
+        document.body.appendChild(ele2);
+        ele2.style.outline = "1px solid black";
+        ele2.width = 115;
+        ele2.height = 115 * 1.3 * 5;
+        let can = ele2.getContext("2d");
+        let pos = this.animitions.stop.pos;
+        for (let i = 0; i < pos.length; ++i) {
+            can.drawImage(this.img, pos[i][0], pos[i][1], this.w, this.h, 0, i * 115 * 1.3, 115, 115 * 1.3);
+        }
+    }
+    stop() {
+        if (this.lock)
+            return;
+        this.animition(this.animitions.stop);
+    }
+    jump() {
+        if (this.lock)
+            return;
+        this.animition(this.animitions.jump);
     }
     turn() {
         this.canvas.translate(this.w, 0);
         this.canvas.scale(-1, 1);
     }
     stand() {
-        if (this.status == 0)
+        if (this.status == 0 || this.lock)
             return;
         this.animition(this.animitions.stand);
         this.status = 0;
     }
     run() {
-        if (this.status == 1)
+        if (this.status == 1 || this.lock)
             return;
         this.animition(this.animitions.run);
         this.status = 1;
--- scripts/play.ts
+++ scripts/play.ts
@@ -2,7 +2,7 @@
 enum keycode {
     KeyA = 0,
     KeyD,
-    Space
+    KeyW
 }
 
 class Player {
@@ -10,7 +10,7 @@ class Player {
     canvas_h: number = 300
     canvas: CanvasRenderingContext2D
     mario: mario //马里奥对象
-    mario_w: number = 100
+    mario_w: number = 50
     mario_h: number = this.mario_w * 1.3 //主角的大小
     mario_way: number = 0
     mario_status: number = 0
@@ -19,7 +19,12 @@ class Player {
     mvspeed: number = 0
     maxspeed: number = 8
     accl: number = 0.2
-    turn:boolean = false
+    turn: boolean = false
+    jumpHeight: number = this.mario_h * 2
+    jumpspeed: number = 0
+    jumpmaxspeed: number = 10
+    jumpaccl: number = 0.5
+    jumpstatus: number = 0
     constructor(ele: string, img: string) {
         const canvas = document.getElementById(ele) as HTMLCanvasElement
         canvas.width = this.canvas_w
@@ -28,89 +33,109 @@ class Player {
         this.mario = new mario()
     }
     start() {
-        let _this = this
-        let keydown: Array<number> = []
-        document.onkeydown = function (e) {
-            let key: string = (e as KeyboardEvent).code
-            switch (key) {
-                case keycode[0]:
-                    if (keydown.indexOf(keycode.KeyA) == -1) {
-                        keydown.unshift(keycode.KeyA)
-                        // if(_this.turn){
-                        //     // _this.mario.turn()
-                        //     _this.turn = false
-                        // }
-                    }
-                    break
-                case keycode[1]:
-                    if (keydown.indexOf(keycode.KeyD) == -1) {
-                        keydown.unshift(keycode.KeyD)
-                        // if(!_this.turn){
-                        //     // _this.mario.turn()
-                        //     _this.turn = true
-                        // }
-                    }
-                    break
-                case keycode[2]:
-                    // keydown.add(keycode.Space)
-                    break
+        var keydown: Array<number> = []
+        {
+            let _this = this
+            document.onkeydown = function (e) {
+                let key: string = (e as KeyboardEvent).code
+                switch (key) {
+                    case keycode[0]:
+                        if (keydown.indexOf(keycode.KeyA) == -1) {
+                            keydown.unshift(keycode.KeyA)
+                            // if(_this.turn){
+                            //     // _this.mario.turn()
+                            //     _this.turn = false
+                            // }
+                        }
+                        break
+                    case keycode[1]:
+                        if (keydown.indexOf(keycode.KeyD) == -1) {
+                            keydown.unshift(keycode.KeyD)
+                            // if(!_this.turn){
+                            //     // _this.mario.turn()
+                            //     _this.turn = true
+                            // }
+                        }
+                        break
+                    case keycode[2]:
+                        _this.jump()
+                        // keydown.add(keycode.Space)
+                        break
+                }
+            }
+            document.onkeyup = function (e) {
+                let key: string = (e as KeyboardEvent).code
+                switch (key) {
+                    case keycode[0]:
+                        keydown.splice(keydown.indexOf(keycode.KeyA), 1)
+                        break
+                    case keycode[1]:
+                        keydown.splice(keydown.indexOf(keycode.KeyD), 1)
+                        break
+                    case keycode[2]:
+                        // keydown.add(keycode.Space)
+
+                        // console.log(key)
+                        return false
+                        break
+                }
+                return false
             }
         }
-        document.onkeyup = function (e) {
-            let key: string = (e as KeyboardEvent).code
-            switch (key) {
-                case keycode[0]:
-                    keydown.splice(keydown.indexOf(keycode.KeyA), 1)
-                    break
-                case keycode[1]:
-                    keydown.splice(keydown.indexOf(keycode.KeyD), 1)
-                    break
-                case keycode[2]:
-                    // keydown.add(keycode.Space)
-                    break
-            }
-        }
+        // document.("keydown",function(e){     
+        //     e=window.event||e;
+
+        //      //禁止空格键翻页 
+        //      if(e.keyCode==32){
+        //         return false; 
+        //      }
+
+        //  });
         let count = 0
         setInterval(() => {
-
             if (keydown.length) {
-                if(this.mario_status == 0){
-                    this.mario.run()
+                if (this.mario_status == 0) {
                     this.mario_status = 1
                 }
-                if(keydown[0] != this.mario_way){
-                    // if(this.turn != Boolean(keydown[0])){   
-                    //     this.mario.turn()
-                    //     this.turn = !this.turn
-                    // }
-                    if(this.mvspeed - this.accl * 2 > 0){   //当刹车时给予两倍的减速度
-                        this.mario_status = 0
+                if (this.mario.status != 1 && this.mario_status != 3) {
+                    this.mario.run()
+                    this.mario.status = 1
+                }
+                if (keydown[0] != this.mario_way) {
+                    if (this.mvspeed - this.accl * 2 > 0) {   //当刹车时给予两倍的减速度
                         this.mvspeed -= this.accl * 2
-                    }else{                                  /* 当惯性消失后再改变方向 */
+                        if (this.mario_status != 3) {
+                            this.mario_status = 2;
+                            if (this.mario.status != 2) {     //给予刹车动画
+                                this.mario.stop()
+                                this.mario.status = 2
+                            }
+                        }
+                    } else {                                  /* 当惯性消失后再改变方向 */
                         this.mario_way = keydown[0]
-                        _this.mario.turn()
+                        this.mario.turn()
+                    }
+                } else {
+                    if (this.mvspeed < this.maxspeed) {
+                        this.mvspeed += this.accl
+                    } else {
+                        this.mvspeed = this.maxspeed
                     }
                 }
             } else {
-                this.mario_status = 0
-            }
-            //如在行进中给予加速度
-            if (this.mario_status) {
-                if (this.mvspeed < this.maxspeed) {
-                    this.mvspeed += this.accl
-                }else{
-                    this.mvspeed = this.maxspeed
-                }
-            } else if(this.mvspeed){
-                if(this.mvspeed - this.accl > 0){
+                if (this.mvspeed - this.accl > 0) {
                     this.mvspeed -= this.accl
                 } else {
                     this.mvspeed = 0
-                    if(keydown.length == 0){
+                    if (keydown.length == 0) {
                         this.mario.stand()
                     }
                 }
+                if (this.mario_status == 1) {
+                    this.mario_status = 0
+                }
             }
+
             //根据惯性的方向设置x
             if (this.mario_way == 0) {
                 this.x >= this.mvspeed ? this.x -= this.mvspeed : this.x = 0
@@ -118,12 +143,50 @@ class Player {
                 this.x < this.canvas_w ? this.x += this.mvspeed : this.x = this.canvas_w
             }
 
-
-
             this.update(count++)
             count %= 60
         }, 1000 / 60)
         this.mario.stand()
+    }
+    jump() {
+        if (this.mario_status == 3) return
+        this.mario_status = 3
+        var inter = setInterval(() => {
+            if (this.mario_status == 3) {
+                if (this.jumpspeed > 0) {
+                    this.jumpspeed + this.jumpaccl < this.jumpmaxspeed ? this.jumpspeed += this.jumpaccl : this.jumpspeed = this.jumpmaxspeed;
+                }
+                else {
+                    if (this.mario.status != 3) {
+                        this.mario.jump();
+                        this.mario.status = 3
+                    }
+                    this.jumpspeed += this.jumpaccl;
+                }
+                if (this.jumpstatus == 0) {
+                    if (this.canvas_h - this.y - this.mario_h - this.jumpspeed < this.jumpHeight) {
+                        this.y -= this.jumpspeed
+                    } else {
+                        this.jumpstatus = 1
+                        this.jumpspeed = 0
+                    }
+                }
+                else {
+                    if (this.canvas_h - this.y - this.mario_h - this.jumpspeed > 0) {
+                        this.y += this.jumpspeed
+                        this.mario.stop()
+                    } else {
+                        this.mario_status = 0
+                        this.jumpstatus = 0
+                        this.jumpspeed = 0
+                        this.mario.stand()
+                        this.y = this.canvas_h - this.mario_h;
+                        clearInterval(inter)
+                    }
+                }
+            }
+        }, 1000 / 60)
+
     }
     update(count: number) {
         //马里奥对象
@@ -143,7 +206,7 @@ class mario {
     img: HTMLImageElement
     canvas: CanvasRenderingContext2D
     object: HTMLCanvasElement
-    turned:boolean = false
+    turned: boolean = false
     animitions = {
         stand: {
             fps: 20,
@@ -152,16 +215,29 @@ class mario {
                 [230, 65]
             ]
         },
-        run:{
-            fps:12,
-            pos:[
-                [720,65],
-                [980,65],
+        run: {
+            fps: 12,
+            pos: [
+                [720, 65],
+                [980, 65],
+            ]
+        },
+        jump: {
+            fps: 10,
+            pos: [
+                [1255, 65]
+            ]
+        },
+        stop: {
+            fps: 60,
+            pos: [
+                [1527, 65]
             ]
         }
     }
-    status:number = 0
+    status: number = 0
     inter: any
+    lock: boolean = false
     constructor() {
         const ele = document.createElement("canvas")
         ele.width = this.w
@@ -173,31 +249,39 @@ class mario {
         this.animition(this.animitions.stand)
 
 
-
         // document.body.appendChild(ele)
-        // //testanimi
-        // let ele2 = document.createElement("canvas")
-        // document.body.appendChild(ele2)
-        // ele2.width = 115
-        // ele2.height = 115 * 1.3 * 5
-        // let can = ele2.getContext("2d") as CanvasRenderingContext2D
-        // let pos = this.animitions.run.pos
-        // for(let i = 0; i < pos.length;++i){
-        //     can.drawImage(this.img,pos[i][0],pos[i][1],this.w,this.h,0,i * 115 * 1.3,115,115 * 1.3)
-        // }   
+        //testanimi
+        let ele2 = document.createElement("canvas")
+        document.body.appendChild(ele2)
+        ele2.style.outline = "1px solid black"
+        ele2.width = 115
+        ele2.height = 115 * 1.3 * 5
+        let can = ele2.getContext("2d") as CanvasRenderingContext2D
+        let pos = this.animitions.stop.pos
+        for (let i = 0; i < pos.length; ++i) {
+            can.drawImage(this.img, pos[i][0], pos[i][1], this.w, this.h, 0, i * 115 * 1.3, 115, 115 * 1.3)
+        }
 
     }
-    turn(){
-        this.canvas.translate(this.w,0)
-        this.canvas.scale(-1,1)
+    stop() {
+        if (this.lock) return
+        this.animition(this.animitions.stop)
+    }
+    jump() {
+        if (this.lock) return
+        this.animition(this.animitions.jump)
+    }
+    turn() {
+        this.canvas.translate(this.w, 0)
+        this.canvas.scale(-1, 1)
     }
     stand() {
-        if(this.status == 0) return
+        if (this.status == 0 || this.lock) return
         this.animition(this.animitions.stand)
         this.status = 0
     }
-    run(){
-        if(this.status == 1) return
+    run() {
+        if (this.status == 1 || this.lock) return
         this.animition(this.animitions.run)
         this.status = 1
     }
@@ -206,7 +290,7 @@ class mario {
         clearInterval(this.inter)
         const _this = this
         let count: number = 0
-        function runthis(){
+        function runthis() {
             _this.canvas.clearRect(0, 0, 300, 300)
             _this.canvas.drawImage(_this.img, animi.pos[count % animi.pos.length][0], animi.pos[count % animi.pos.length][1], _this.w, _this.h,
                 0, 0, _this.w, _this.h)
